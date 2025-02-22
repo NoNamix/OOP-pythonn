537f29202b7dc92797a0fa8cacf9bb40daea7522
NoNamix <lopohavr@gmail.com>
2025-02-22 17:35:58 +0200

    lesson10

---
+++ lesson10.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+connection = sqlite3.connect('itstep_DB.sl3', 5)
+cur = connection.cursor()
+cur.execute("CREATE TABLE first_table (name TEXT);")
+connection.commit()
+# print(connection)
+# print(cur)
+connection.close()
